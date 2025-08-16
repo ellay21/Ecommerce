713cefde0ececd664a7e686ea5edf2c1759cbd10
ellay21 <mesudmelaku1@gmail.com>
2025-08-17 00:43:26 +0300

    implemented feature Product Management API

---
+++ product-service/products/urls.py
@@ -0,0 +1,9 @@
+# product_service/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+]
