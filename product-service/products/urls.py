17987077b3c14ee348cc56251728f1d0880caf8f
Mesud Melaku <149295529+ellay21@users.noreply.github.com>
2025-09-09 20:13:37 +0300

    refactor(architecture): from microservice to multi layer architecture

--- product-service/products/urls.py
+++
@@ -1,9 +0,0 @@
-# product_service/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
-]
--- product-service/products/views.py
+++
@@ -1,9 +0,0 @@
-# products/views.py
-
-from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
